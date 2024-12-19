64086bf838f20fe53078cc0ca094a5831b4c2bd5
Tyler Smoot <69122799+tylersmoot@users.noreply.github.com>
2024-12-19 17:24:16 -0600

    Add files via upload

--- library/book_library.py
+++ library/book_library.py
@@ -12,15 +12,15 @@ current_time_seconds = current_time.strftime('%S')
 
 timestamp = current_time_hour + ': ' + current_time_minutes + ': ' + current_time_seconds
 
-log_transaction = open('./database/library_transactions.txt', 'a')
+def showBooks():
+   print('** LIBRARY **')
+   print(books)
+
 
 books = pd.read_csv('./database/dataset.csv')
-print(books.to_string())
+showBooks()
 
 print()
-
-def showBooks():
-   print(books.to_string())
 
 user_input = ''
 user_selection_check_in = ''
@@ -28,7 +28,7 @@ user_selection_check_in = ''
 
 while(user_input != 0):
    
-   user_input = int(input('Choose an option: \n'
+   user_input = int(input('What would you like to do?\n\n'
                    '(1) Check Out A Book\n'
                    '(2) Check In A Book\n'
                    '(0) Exit\n'))
@@ -36,60 +36,65 @@ while(user_input != 0):
    
    if (user_input == 1):
       print('Which book would you like to check out?')
+
       showBooks()
 
       print()
-      print('Choose a book id to check out')
+      print('Choose a book ID to check OUT')
 
       user_book_choice = int(input())
       found_book = books.loc[user_book_choice]
+      
+
       print()
       print('****** Book Found: ******')
       print(found_book)
 
-      found_book_status = books.loc[user_book_choice, 'status']
-      found_book_title = books.loc[user_book_choice, 'title']
-      found_book_id = books.loc[user_book_choice,'book_id']
-      found_book_id = str(found_book_id)
-
+      found_book_status = books.at[user_book_choice, 'status']
+      found_book_title = books.at[user_book_choice, 'title']
+      found_book_id = books.at[user_book_choice,'book_id']
+     
+      
       print()
       print(f'This book is currently: {found_book_status}')
       print()
 
-      if(found_book_status != 'In'):
-         print('This book is already checked out')
-      
-      else:
-         user_confirmation = int(input(f'Confirm: Do you want to check {found_book_title} out?\n'
+      if(found_book_status != 'Out'):
+         
+     
+         user_confirmation = int(input(f'Confirm: Do you want to check {found_book_title} out?\n\n'
                               '(1) YES\n'
                               '(2) NO\n'))
 
          if (user_confirmation == 1):
             books.loc[user_book_choice, 'status'] = 'Out'
             books.to_csv('D:/python/projects/library/database/dataset.csv', index=False)
+            log_transaction = open('./database/library_transactions.txt', 'a')
             showBooks()
             print()
-            log_transaction.write('Occurence: Out,' + ' ' + 'Time: ' + timestamp + ', Book Title: ' + found_book_title + ', Book ID: ' + found_book_id + '\n')
+            log_transaction.write('Occurence: Out,' + ' ' + 'Time: ' + str(timestamp) + ', Book Title: ' + found_book_title + ', Book ID: ' + str(found_book_id) + '\n')
             print('** TRANSACTIONS **')
             log_transaction = open('./database/library_transactions.txt', 'r')
             print(log_transaction.read())
-            continue
+            break
+         
+            
+            
 
          elif (user_confirmation == 2):
             break
 
    if(user_input == 2):
       showBooks()
-      print('Which book would you like to check in?')
+      print('Choose a book ID to check IN')
+
       user_book_choice = int(input())
-      print(user_book_choice)
       found_book = books.loc[user_book_choice]
-      found_book_status = books.loc[user_book_choice, 'status']
-      found_book_title = books.loc[user_book_choice, 'title']
-      found_book_id = books.loc[user_book_choice, 'book_id']
+      found_book_status = books.at[user_book_choice, 'status']
+      found_book_title = books.at[user_book_choice, 'title']
+      found_book_id = found_book.at['book_id']
+     
     
-      
-      
       print()
       print('****** Book Found: ******')
       print(found_book)
@@ -102,14 +107,12 @@ while(user_input != 0):
       if (user_confirmation == 1):
          books.loc[user_book_choice, 'status'] = 'In'
          books.to_csv('D:/python/projects/library/database/dataset.csv', index=False)
-        
-         log_transaction.write('Occurence: In,' + ' ' + 'Time: ' + timestamp + ', Book Title: ' + found_book_title + ', Book ID: ' + found_book_id + '\n')
+         log_transaction = open('./database/library_transactions.txt', 'a')
+         log_transaction.write('Occurence: In,' + ' ' + 'Time: ' + str(timestamp) + ', Book Title: ' + str(found_book_title) + ', Book ID: ' + str(found_book_id) + '\n')
          print('** TRANSACTIONS **')
          log_transaction = open('./database/library_transactions.txt', 'r')
          print(log_transaction.read())
+         break
+       
    
-
-      continue
-
 log_transaction.close()
-
